7ad5c2b93b30d186412aa29fcfa222395bcd1d8b
jason89923 <jason89923@gmail.com>
2022-07-28 17:40:54 +0800

    Add location record

--- src/joy_twist.cpp
+++ src/joy_twist.cpp
@@ -3,50 +3,131 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 
+#include <vector>
 
 using namespace std;
 
-class Car_control {
-    private:
-    static int gear;
-    static bool ready;
+class Trigger {
+   private:
+    int previousValue;
+
    public:
-    geometry_msgs::Twist control(float angular, float vol_forward, float vol_backward, int lb, int rb) {
-        geometry_msgs::Twist twist;
-        if (lb == 0 && rb == 0) {
-            ready = true;
+    Trigger() {
+        previousValue = 0;
+    }
+
+    bool isActive(int currentValue) {
+        bool returnValue = false;
+        if (previousValue == 1 && currentValue == 0) {
+            returnValue = true;
         }
 
-        if (ready) {
+        previousValue = currentValue;
+        return returnValue;
+    }
+};
+
+class Argument {
+   public:
+    int gear;
+    bool ready;
+    vector<pair<float, float> > record;
+    Trigger trigger_L_bottom, trigger_R_bottom, trigger_B_bottom;
+    bool marco_REC;
+    bool back;
+    Argument() {
+        marco_REC = false;
+        back = false;
+    }
+};
+
+class Car_control {
+   private:
+    static Argument argument;
+    static int gearNum;
+
+   public:
+    Car_control() {
+        gearNum = 5;
+    }
+
+    geometry_msgs::Twist control(ros::Publisher publisher, float angular, float vol_forward, float vol_backward, int lb, int rb, int LB, int RB, int B) {
+        geometry_msgs::Twist twist;
+        if (lb == 0 && rb == 0) {
+            argument.ready = true;
+        }
+
+        if (argument.ready) {
             if (lb ^ rb) {
                 if (lb == 1) {
-                    gear--;
+                    argument.gear--;
                 } else {
-                    gear++;
+                    argument.gear++;
                 }
 
-                ready = false;
+                argument.ready = false;
             }
         }
 
-        if (gear < 0) {
-            gear = 0;
-        } else if (gear >= 3) {
-            gear = 2;
+        if (argument.gear < 0) {
+            argument.gear = 0;
+        } else if (argument.gear >= gearNum) {
+            argument.gear = gearNum - 1;
         }
 
-        twist.linear.x = -(vol_forward - vol_backward) * 2.5 * (gear + 1) / 3;
-        twist.angular.z = angular * 2.5 * (gear + 1) / 3;
+        twist.linear.x = -(vol_forward - vol_backward) * 0.11 * (argument.gear + 1) / gearNum;
+        twist.angular.z = angular * 0.45 * (argument.gear + 1) / gearNum;
+
+        bool L_bottom_state = argument.trigger_L_bottom.isActive(LB);
+        bool R_bottom_state = argument.trigger_R_bottom.isActive(RB);
+        bool B_bottom_state = argument.trigger_B_bottom.isActive(B);
+        if (L_bottom_state) {
+            ROS_INFO("LLL");
+            if (argument.marco_REC)
+                argument.marco_REC = false;
+            else
+                argument.marco_REC = true;
+        }
+
+        if (R_bottom_state) {
+            ROS_INFO("RRR");
+            argument.record.clear();
+        }
+
+        if (B_bottom_state) {
+            ROS_INFO("BBB");
+            argument.back = true;
+        }
+
+        if (argument.back) {
+            if (argument.record.size() > 0) {
+                ROS_INFO("size = %d", argument.record.size());
+                int index = argument.record.size() - 1;
+                twist.linear.x = argument.record[index].first;
+                twist.angular.z = argument.record[index].second;
+                argument.record.erase(argument.record.begin() + index);
+                return twist;
+            } else {
+                argument.back = false;
+                ROS_INFO("end");
+            }
+        } else {
+            if (abs(twist.linear.x) > 0.01 || abs(twist.angular.z) > 0.1)
+                argument.record.push_back(move(pair<float, float>(-twist.linear.x, -twist.angular.z)));
+        }
+
         return twist;
     }
 };
 
-int Car_control::gear = 0;
-bool Car_control::ready = false;
+int Car_control::gearNum;
 
+Argument Car_control::argument;
 class Joy_rosky {
    private:
     int RT_index, LT_index, axis_index, LB_index, RB_index;
+
+    int L_bottom, R_bottom, B;
 
    public:
     Joy_rosky() {
@@ -56,18 +137,20 @@ class Joy_rosky {
         nodePtr.param<int>("axis_index", axis_index, 0);
         nodePtr.param<int>("LB_index", LB_index, 4);
         nodePtr.param<int>("RB_index", RB_index, 5);
-
+        nodePtr.param<int>("L_bottom", L_bottom, 6);
+        nodePtr.param<int>("R_bottom", R_bottom, 7);
+        nodePtr.param<int>("B", B, 1);
         // create a publisher that will advertise on the command_velocity topic of the turtle
         publisher = nodePtr.advertise<geometry_msgs::Twist>("/rosky/cmd_vel", 1);
 
         // subscribe to the joystick topic for the input to drive the turtle
-        subscriber = nodePtr.subscribe<sensor_msgs::Joy>("joy", 10, &Joy_rosky::joyCallback, this);
+        subscriber = nodePtr.subscribe<sensor_msgs::Joy>("joy", 1, &Joy_rosky::joyCallback, this);
     }
 
    private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
         Car_control controller;
-        publisher.publish(controller.control(joy->axes[axis_index], (-joy->axes[RT_index] + 1) / 2, (-joy->axes[LT_index] + 1) / 2, joy->buttons[LB_index], joy->buttons[RB_index]));
+        publisher.publish(controller.control(publisher, joy->axes[axis_index], (-joy->axes[RT_index] + 1) / 2, (-joy->axes[LT_index] + 1) / 2, joy->buttons[LB_index], joy->buttons[RB_index], joy->buttons[L_bottom], joy->buttons[R_bottom], joy->buttons[B]));
     }
 
     ros::Publisher publisher;
@@ -77,6 +160,5 @@ class Joy_rosky {
 int main(int argc, char** argv) {
     ros::init(argc, argv, "joy_rosky");
     Joy_rosky joy_rosky;
-
     ros::spin();
 }
